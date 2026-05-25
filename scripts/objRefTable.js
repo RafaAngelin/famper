711b73040af37fa5f1fb7b7fba39c1691013d19b
Rafael Angelin <77171111+RafaAngelin@users.noreply.github.com>
2026-05-25 20:34:49 -0300

    Add files via upload

--- scripts/objRefTable.js
+++ scripts/objRefTable.js
@@ -4,18 +4,29 @@ self.C3_GetObjectRefTable = function () {
 		C3.Plugins.Sprite,
 		C3.Behaviors.Platform,
 		C3.Behaviors.bound,
-		C3.Behaviors.solid
+		C3.Behaviors.solid,
+		C3.Plugins.Touch,
+		C3.Plugins.Touch.Cnds.IsTouchingObject,
+		C3.Behaviors.Platform.Acts.SimulateControl
 	];
 };
 self.C3_JsPropNameTable = [
 	{Plataforma: 0},
 	{RestritoAoLayout: 0},
-	{Sprite: 0},
+	{player_colisao: 0},
 	{Sólido: 0},
-	{Sprite2: 0}
+	{chao: 0},
+	{esq: 0},
+	{dir: 0},
+	{jump: 0},
+	{Toque: 0}
 ];
 
 self.InstanceType = {
-	Sprite: class extends self.ISpriteInstance {},
-	Sprite2: class extends self.ISpriteInstance {}
+	player_colisao: class extends self.ISpriteInstance {},
+	chao: class extends self.ISpriteInstance {},
+	esq: class extends self.ISpriteInstance {},
+	dir: class extends self.ISpriteInstance {},
+	jump: class extends self.ISpriteInstance {},
+	Toque: class extends self.IInstance {}
 }
